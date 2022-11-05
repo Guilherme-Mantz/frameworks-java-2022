2ea0285a91a4df0ff2f32979a30b1bdd79616a22
Guilherme Mantz <89493263+Guilherme-Mantz@users.noreply.github.com>
2022-11-05 14:41:25 -0300

    Delete src directory

--- src/main/java/br/com/bluesoft/alugar/repository/AluguelRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.bluesoft.alugar.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.bluesoft.alugar.modelo.Aluguel;
-
-public interface AluguelRepository extends CrudRepository<Aluguel, Integer> {
-
-}
